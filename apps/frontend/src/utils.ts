97416e0013a7868edfacf4347c4d1db4181b550c
hallvard-hetlelid <hallvard@unimicro.no>
2025-11-25 19:09:25 +0100

    Add score normalization to Teams game

--- apps/frontend/src/utils.ts
+++ apps/frontend/src/utils.ts
@@ -1,20 +1,3 @@
-
-export interface GameScorePercentiles {
-    // Zero points scored
-    loss: 0,
-    // Barely makes the cut
-    low: number,
-    // Not too bad
-    medium: number,
-    // Good, could be better
-    good: number,
-    // Getting this many points nets the ordinary 100% score
-    full: number,
-    // Gettings this many points gives you more then 100% score
-    beyond_full: number,
-
-}
-
 export function normalizeGameScore(score: number, maxScore: number, timeBonusPercentage?: number): number {
     const calculatedTimeBonus = timeBonusPercentage ? timeBonusPercentage * 200 : 0;
     return (score / maxScore * 1000) + calculatedTimeBonus;
